0a4d3f2f0c7e95955b8647fae7f6bd45c43784e1
itpark <a.m.smyslov@yandex.ru>
2023-02-26 20:46:22 +0300

    done guess number

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import telebot
 import random
 from datetime import datetime
 
+comp_number = -1
+user_number = -1
+
 def log(message):
     print(f"LOG {datetime.now()} --- {message}")
 
@@ -10,6 +13,9 @@ log("BOT STARTED")
 
 @bot.message_handler(content_types=["text"])
 def start(user_message):
+    global comp_number
+    global user_number
+
     chat_id = user_message.chat.id
     request = user_message.text
     response = ""
@@ -17,9 +23,19 @@ def start(user_message):
     log(f"FROM {chat_id} received {request}")
 
     if request == '/start':
-        response = "вы прислали команду старт"
+        comp_number = random.randint(1,100)
+        response = "бот загадал число от 1 до 100, попробуйте отгадать это число"
     else:
-        response = "неизвестная команда введите /start"
+        if request.isdecimal() == True:
+            user_number = int(request)
+            if user_number>comp_number:
+                response = "введите поменьше"
+            elif user_number<comp_number:
+                response = "введите побольше"
+            else:
+                response = "вы угадали, введите /start для начала новой игры "
+        else:
+            response = "ошибка вы ввели не число"
 
     bot.send_message(chat_id, response)
 
